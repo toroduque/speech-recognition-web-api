3da82cb0403d0907cdbc1f850c05069ae23dc2cf
toroduque <toro.duque@gmail.com>
2020-11-03 12:34:16 +1100

    First commit: Concept idea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Transcript } from './styled'
 
 class App extends Component {
+
+  state = {
+    recognition: null,
+    transcripts: [],
+    indexCounter: 0
+  }
+
+  componentDidMount = () => {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    const recognition = new SpeechRecognition()
+    recognition.interimResults = true
+    recognition.lang = 'en-AU';
+
+    this.setState({recognition})
+  }
+
+  startRecognition = () => {
+    const { recognition } = this.state
+
+    recognition.addEventListener('result', e => {
+      const currentTranscript = Array.from(e.results)
+        .map(result => {
+          if(result.isFinal) return result[0]
+        })
+        .map(result => {
+          if(result && result.confidence > 0.70 ) return result.transcript
+        })
+        .join('')
+
+      const transcripts = [...this.state.transcripts, currentTranscript]
+      this.setState({transcripts})
+    })
+
+    recognition.start()
+    recognition.onspeechend = () => this.setState(prevState => ({ indexCounter: prevState.indexCounter + 1 }))
+    recognition.addEventListener('end', recognition.start)
+  }
+
+  stopRecognition = () => {
+    const { recognition } = this.state
+
+    recognition.abort()
+  }
+
+
   render() {
+    const { recognition, transcripts, indexCounter } = this.state 
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { recognition && <button onClick={this.startRecognition}> Start Speech SpeechRecognition</button>}
+        <Transcript> 
+          { transcripts.map((transcript, index) => <p key={index}>{transcript}</p>) }
+        </Transcript>
       </div>
     );
   }
---
+++ src/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Transcript = styled.span`
+    margin: 10px;
+    padding: 10px;
+    font-size: 26px;
+    line-height: 30px;
+`
